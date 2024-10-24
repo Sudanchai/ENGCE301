4b293a659e17ca224905b0499f399de46d0b2a3e
Danchai <sudanchai.1@gmail.com>
2024-10-24 22:26:42 +0700

    [update]

--- LAB4/my-app/src/components/DisplayBoard.js
+++ LAB4/my-app/src/components/DisplayBoard.js
@@ -1,25 +1,25 @@
 import React from 'react'
 
-export const DisplayBoard = ({numberOfUsers, getAllUsers}) => {
+export const DisplayBoard = ({numberOfMovies, getAllMovies}) => {
 
-    const headerStyle = {
+   const headerStyle = {
 
-        width: '100%',
-        padding: '2%',
-        backgroundColor: "red",
-        color: 'white',
-        textAlign: 'center'
-    }
-    
-    return(
-        <div style={{backgroundColor:'green'}} className="display-board">
-            <h4 style={{color: 'white'}}>Users Created</h4>
-            <div className="number">
-            {numberOfUsers}
-            </div>
-            <div className="btn">
-                <button type="button" onClick={(e) => getAllUsers()} className="btn btn-warning">Get all Users</button>
-            </div>
-        </div>
-    )
+       width: '100%',
+       padding: '2%',
+       backgroundColor: "red",
+       color: 'white',
+       textAlign: 'center'
+   }
+  
+   return(
+       <div style={{backgroundColor:'green'}} className="display-board">
+           <h4 style={{color: 'white'}}>Movies Created</h4>
+           <div className="number">
+           {numberOfMovies}
+           </div>
+           <div className="btn">
+               <button type="button" onClick={(e) => getAllMovies()} className="btn btn-warning">Get all Movies</button>
+           </div>
+       </div>
+   )
 }
--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 [Lab3 google doc](https://docs.google.com/document/d/1BECOrJYk7-P4bsDnCfCM6NSYkaZ9hjTBBCpqFPqxfZ0/edit?usp=sharing)
 
-[Lab4 google doc](https://docs.google.com/document/d/1BECOrJYk7-P4bsDnCfCM6NSYkaZ9hjTBBCpqFPqxfZ0/edit?usp=sharing)
+[Lab4 google doc](https://docs.google.com/document/d/1-KTJMwBnFGMG3UyG0PQ1DEXzFv7aZDPxHewgYgsv-ao/edit?usp=sharing)
 
 [Lab5 google doc](https://docs.google.com/document/d/1yarUOj99CABfb8CcBRoHnIKXzrNJpo29PvRHUGMi4Jw/edit?usp=drive_link)
 
